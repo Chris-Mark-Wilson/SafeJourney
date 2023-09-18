8a81069ddb7b64fc984f7b3999dbb6e0f22a0cac
bigsegs <cw.plastering.services@gmail.com>
2023-09-18 13:30:14 +0100

    extracted appStyle stylesheet to separtate file

---
+++ styles/appStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+export const appStyle = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
