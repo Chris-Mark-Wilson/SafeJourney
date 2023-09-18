e912f1a4b8818b7c2f7ea020946dc22e4a37e908
GemboJones <gembojones@gmail.com>
2023-09-18 02:40:57 +0100

    added side bar + home component

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-
+import { appStyle } from './styles/appStyle';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+
+import { Home } from './components/Home';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={appStyle.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
