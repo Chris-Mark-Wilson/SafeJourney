131d8329aba15547e12d74fba0ef3125b75479c5
Aminah <aminahpsaleem@gmail.com>
2023-09-19 10:53:04 +0100

    added header to the top of the page

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import Header from './components/Header';
 
 import { appStyle } from './styles/appStyle';
 import { StatusBar } from 'expo-status-bar';
@@ -13,9 +14,13 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+       <View>
+        <Header />
+      </View>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Home} />
       </Drawer.Navigator>
+      
     </NavigationContainer>
   );
 }
---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Safe Journey</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default Header;
