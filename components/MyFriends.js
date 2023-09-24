83a35ca39c9659d4e007675d75af7ec3b1be1085
Chris-Lupton <kitlupton@hotmail.com>
2023-09-24 14:04:53 +0100

    adds filter to friends list

--- components/MyFriends.js
+++ components/MyFriends.js
@@ -3,14 +3,16 @@ import {
   Text,
   FlatList,
   Pressable,
-  SectionList,
+  Alert,
   StyleSheet,
+  ActivityIndicator
 } from "react-native";
 import { appStyle } from "../styles/appStyle";
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../context/userContext";
 import { FriendContext } from "../context/friendContext";
 import { getFriends } from "../utils/api";
+import * as Notifications from 'expo-notifications';
 
 const Item = ({ name }) => (
   <View style={styles.name}>
@@ -23,53 +25,86 @@ export const MyFriends = ({ navigation }) => {
   const { friendData, setFriendData } = useContext(FriendContext);
 
   const [friends, setFriends] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setTimer(timer+1)
+      },10000)
+  },[timer])
 
   useEffect(() => {
-    setIsLoading(true);
+    if(!friends.length)setIsLoading(true)
     getFriends(userData.user_id)
       .then((friendList) => {
-        setFriends(() => {
-          return [...friendList];
-        });
+
+        console.log('friend list updated');
         setIsLoading(false);
+
+        if(friends.length){
+          friendList.forEach((friend, index) => {
+            if(friend.location.status !== friends[index].location.status){
+              if(friend.location.status){
+                schedulePushNotification(`${friend.name} has started their journey`)
+                showAlert(`${friend.name} has started their journey`)
+              }
+              if(!friend.location.status){
+                schedulePushNotification()
+                showAlert(`${friend.name} has ended their journey`)
+              }
+            }
+          })
+        }
+
+        setFriends(friendList);
       })
       .catch((err) => {
-        console.log(err),"err in catch Myfriends";
+        console.log(err)
       });
-  }, []);
+  }, [timer, userData]);
 
   const handlePress = (val) => {
-    console.log(val);
-
-    setFriendData((friend) => {
-      let newData = { ...val };
-
-      return newData;
-    });
+    setFriendData({ ...val });
     navigation.navigate('Home');
   };
 
+  function showAlert(msg) {
+    Alert.alert(msg)
+  }
+
+  function schedulePushNotification(msg) {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: msg,
+        data: { data: 'goes here' },
+      },
+      trigger: null,
+    });
+  }
+
   return isLoading ? (
-    <Text>Loading</Text>
+    <ActivityIndicator size="large" color="grey" />
   ) : (
     <View style={appStyle.container}>
       <Text>My Friends</Text>
       <FlatList
-        data={friends}
-        renderItem={({ item }) =>
-          item.location.status === true ? (
+        data={friends.filter(friend => friend.location.status)}
+        renderItem={({ item }) => (
             <Pressable
               onPress={() => handlePress(item)}
               style={appStyle.pressable}
             >
               <Item name={item.name} />
             </Pressable>
-          ) : (
-            <Item name={item.name} />
-          )
+          ) 
         }
         keyExtractor={(item) => item.name}
+      />
+      <FlatList
+        data={friends.filter(friend => !friend.location.status)}
+        renderItem={({ item }) => <Item name={item.name} />}
       />
     </View>
   );
